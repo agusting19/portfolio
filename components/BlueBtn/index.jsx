228fa10f3548e8348af696994a83afcbaed07aa6
Agustin Gomez <agusleongomez@gmail.com>
2023-04-06 21:24:36 -0300

    added cv link

--- components/BlueBtn/index.jsx
+++ components/BlueBtn/index.jsx
@@ -1,10 +1,10 @@
-import { AiOutlineMail } from 'react-icons/ai';
-import { EMAIL } from '../../constants';
-import styles from './btn.module.scss';
+import { AiOutlineMail } from "react-icons/ai";
+import { CV_URL } from "../../constants";
+import styles from "./btn.module.scss";
 
 const BlueBtn = () => {
   return (
-    <a href={`mailto:${EMAIL}`} rel="noopener nofollow" className={styles.mailBtn}>
+    <a href={CV_URL} target="_blank" rel="noopener nofollow" className={styles.mailBtn}>
       <p>CONTACT ME</p>
       <AiOutlineMail />
     </a>
--- constants/urls.js
+++ constants/urls.js
@@ -1,3 +1,4 @@
 export const GITHUB_URL = 'https://github.com/agusting19';
 export const LINKEDIN_URL = 'https://www.linkedin.com/in/agustin-gomez-602511236/';
 export const EMAIL = 'agusleongomez@gmail.com';
+export const CV_URL = 'https://drive.google.com/file/d/1d_5NuU4_lw9nVYLEG3muUnC2nYeQc4MA/view?usp=sharing'
