ee4140a87e8d8d65c9584d620b368285a13b1932
Agustin Gomez <agusleongomez@gmail.com>
2023-04-12 02:13:27 -0300

    fix contact btn

--- components/BlueBtn/index.jsx
+++ components/BlueBtn/index.jsx
@@ -1,10 +1,10 @@
 import { AiOutlineMail } from "react-icons/ai";
-import { CV_URL } from "../../constants";
+import { EMAIL } from "../../constants";
 import styles from "./btn.module.scss";
 
 const BlueBtn = () => {
   return (
-    <a href={CV_URL} target="_blank" rel="noopener nofollow" className={styles.mailBtn}>
+    <a href={`mailto:${EMAIL}`} rel="noopener nofollow" className={styles.mailBtn}>
       <p>CONTACT ME</p>
       <AiOutlineMail />
     </a>
