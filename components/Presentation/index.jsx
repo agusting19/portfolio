f4a0bd3ef2de86844a8d4bb5ea08fb6625777970
Agustin Gomez <agusleongomez@gmail.com>
2023-06-27 16:09:14 -0300

    styled presentation component

--- components/Presentation/index.jsx
+++ components/Presentation/index.jsx
@@ -1,18 +1,28 @@
+import Image from "next/image";
 import Btn from "../Btn";
 import styles from "./presentation.module.scss";
 
 const Presentation = () => {
   return (
     <section className={styles.container}>
-      <h1 className={styles.name}>AGUSTIN GOMEZ</h1>
-      <p className={styles.title}>
-        Full Stack Web Developer passionate about technology and programming.
-      </p>
-      <p className={styles.subTitle}>
-        Always learning new technologies and looking for new ways to improve
-        projects.
-      </p>
-      <Btn />
+      <div className={styles.textContainer}>
+        <h1 className={styles.name}>AGUSTIN GOMEZ</h1>
+        <p className={styles.title}>
+          Full Stack Developer passionate about technology and programming.
+        </p>
+        <p className={styles.subTitle}>
+          Always learning new technologies and looking for new ways to improve
+          projects.
+        </p>
+        <Btn />
+      </div>
+      <Image
+        src="/me.webp"
+        alt="Foto de frente"
+        width={400}
+        height={400}
+        className={styles.photo}
+      />
     </section>
   );
 };
