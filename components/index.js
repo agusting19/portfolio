d374fbde278eed86ed9674c09082a72691fdfabc
Agustin Gomez <agusleongomez@gmail.com>
2023-06-28 23:19:56 -0300

    created experience component

--- components/index.js
+++ components/index.js
@@ -1,5 +1,7 @@
 export { default as Btn } from "./Btn";
+export { default as Experience } from "./Experience";
 export { default as Header } from "./Header";
 export { default as Presentation } from "./Presentation";
 export { default as ProjectCard } from "./ProjectCard";
 export { default as Projects } from "./Projects";
+export { default as StudentHatIcon } from "./StudentHatIcon";
