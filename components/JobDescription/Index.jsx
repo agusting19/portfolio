7f6da577b35120ea39c72fef4e08e748e7b72d71
Agustin Gomez <agusleongomez@gmail.com>
2023-06-29 11:37:44 -0300

    little fixes

--- components/JobDescription/Index.jsx
+++ components/JobDescription/Index.jsx
@@ -3,14 +3,14 @@ import styles from "./job.module.scss";
 const JobDescription = ({ jobTitle, company, period, techs }) => {
   return (
     <div className={styles.job}>
-      <div class={styles.spinner}></div>
+      <div className={styles.spinner}></div>
       <div>
         <h3>{jobTitle}</h3>
         <p className={styles.company}>{company}</p>
         <p className={styles.period}>{period}</p>
         <ul className={styles.techs}>
-          {techs.map((tech) => {
-            return <li>{tech}</li>;
+          {techs.map((tech, index) => {
+            return <li key={index}>{tech}</li>;
           })}
         </ul>
       </div>
