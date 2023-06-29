8b40a3688b0c76f56e580fc8ee9423a7bafa454b
Agustin Gomez <agusleongomez@gmail.com>
2023-06-28 23:19:00 -0300

    added education component

---
+++ components/EducationDescription/index.jsx
@@ -0,0 +1,17 @@
+import StudentHatIcon from "../StudentHatIcon";
+import styles from "./education.module.scss";
+
+const EducationDescription = ({ title, institution, period }) => {
+  return (
+    <div className={styles.container}>
+      <StudentHatIcon />
+      <div className={styles.textContainer}>
+        <h3>{title}</h3>
+        <p>{institution}</p>
+        <p>{period}</p>
+      </div>
+    </div>
+  );
+};
+
+export default EducationDescription;
