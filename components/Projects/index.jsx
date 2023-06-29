0a71a1accd976b0b3240e7718e6bece5261da85f
Agustin Gomez <agusleongomez@gmail.com>
2023-06-29 20:09:51 -0300

    restyled projects component

--- components/Projects/index.jsx
+++ components/Projects/index.jsx
@@ -1,22 +1,22 @@
-import ProjectCard from '../ProjectCard';
-import projects from '../../constants/Projects.json';
-import styles from './projects.module.scss';
+import ProjectCard from "../ProjectCard";
+import projects from "../../constants/Projects.json";
+import styles from "./projects.module.scss";
 
 const Projects = () => {
   return (
     <section className={styles.container}>
-      <h2>Projects</h2>
+      <h2 className={styles.title}>Projects</h2>
       <div className={styles.cardsContainer}>
         {projects.map((project, index) => {
           return (
             <ProjectCard
               key={index}
-              imgURL={project.imgURL}
               title={project.title}
-              description={project.description}
+              imageUrl={project.imgURL}
               pageUrl={project.pageUrl}
               repoUrl={project.repoUrl}
-              createdWith={project.createdWith}
+              techs={project.techs}
+              description={project.description}
             />
           );
         })}
