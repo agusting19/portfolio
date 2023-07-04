9eac5a3a194729aabf5571fd5462b417ecb4cb80
Agustin Gomez <agusleongomez@gmail.com>
2023-07-04 12:45:44 -0300

    remove analitycs

--- pages/_document.js
+++ pages/_document.js
@@ -1,26 +1,9 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <Script
-          strategy="lazyOnload"
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}
-        />
-
-        <Script id="ga-script" strategy="lazyOnload">
-          {`
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', '${process.env.GOOGLE_ANALYTICS}', {
-      page_path: window.location.pathname,
-    });
-        `}
-        </Script>
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
