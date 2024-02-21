a86bddde84acbf44e429f45dc91c81b4f83dafc5
Agustin Gomez <agusleongomez@gmail.com>
2024-02-21 19:51:57 -0300

    Update ProjectsList styling

--- src/_components/ProjectsList.tsx
+++ src/_components/ProjectsList.tsx
@@ -3,7 +3,7 @@ import ProjectCard from "./ProjectCard"
 
 const ProjectsList = () => {
   return (
-    <section className="max-w-[1100px] py-16 px-auto bg-background md:py-20 md:px-16">
+    <section className="max-w-[1100px] py-16 px-auto bg-backgroundSecondary mx-auto md:py-20 md:px-16">
       <h2 className="text-4xl mb-6 text-lightGray md:text-5xl">Projects</h2>
       <div className="flex flex-col justify-between gap-12 md:gap-20">
         {projects.map((project, index) => {
