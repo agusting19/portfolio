18702d8a4d776ca0b5ff3826624dfd0e4eb36531
Agustin Gomez <agusleongomez@gmail.com>
2024-02-21 19:37:15 -0300

    Update CVDownloadLink styling and remove unnecessary button element

--- src/_components/CvDownloadLink.tsx
+++ src/_components/CvDownloadLink.tsx
@@ -6,9 +6,9 @@ const CvDownloadLink = () => {
       href="/Gomez-Agustin.pdf"
       target="_blank"
       rel="noopener noreferrer"
-      className="font-semibold py-4 px-8 bg-transparent border-2 border-purple-600 transition-all duration-250 text-white cursor-pointer hover:shadow-[inset_0_0_0_2em_#a855f7]"
+      className="font-semibold py-4 px-8 block w-40 bg-transparent border-2 border-purple-600 transition-all duration-250 text-white text-sm cursor-pointer hover:shadow-[inset_0_0_0_2em_#a855f7]"
     >
-      <button>Download CV</button>
+      Download CV
     </Link>
   )
 }
