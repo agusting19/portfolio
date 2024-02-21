3e06166389b804ddb0ddc9272c908b3be7925adc
Agustin Gomez <agusleongomez@gmail.com>
2024-02-21 19:37:09 -0300

    Refactor Presentation component styles

--- src/_components/Presentation.tsx
+++ src/_components/Presentation.tsx
@@ -4,14 +4,14 @@ import CvDownloadLink from "./CvDownloadLink"
 const Presentation = () => {
   return (
     <section className="flex flex-col gap-20 w-11/12 max-w-[1100px] my-16 mx-auto md:flex-row md:justify-center md:items-center md:gap-2 md:my-24 md:p-4">
-      <div className="md:w-3/4">
-        <h1 className="text-5xl leading-none mb-4 custom-gradient text-transparent tracking-tighter">
+      <div className="md:w-3/4 space-y-4">
+        <h1 className="text-5xl font-semibold leading-none mb-4 custom-gradient text-transparent tracking-tighter">
           AGUSTIN GOMEZ
         </h1>
-        <p className="text-3xl text-white md:max-w-[40ch]">
+        <p className="text-3xl text-white md:max-w-[40ch] leading-none">
           Full Stack Developer passionate about technology and programming.
         </p>
-        <p className="my-4 text-lightGray">
+        <p className="my-4 text-lightGray max-w-sm leading-none">
           Always learning new technologies and looking for new ways to improve
           projects.
         </p>
@@ -22,7 +22,7 @@ const Presentation = () => {
         alt="Foto de frente"
         width={1080}
         height={720}
-        className="w-full mx-auto border bg-[#334155] custom-border-radius md:w-1/3"
+        className="w-full mx-auto p-4 bg-[#334155] custom-border-radius md:w-[320px]"
       />
     </section>
   )
