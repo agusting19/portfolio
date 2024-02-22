d39615f72d1ef2b00478c70261e78d76ddc6b757
Agustin Gomez <agusleongomez@gmail.com>
2024-02-22 12:10:20 -0300

    Add job experiences, education details, and certifications details

---
+++ src/constants/experience.ts
@@ -0,0 +1,72 @@
+export const jobExperiences = [
+  {
+    jobTitle: "Fullstack Developer",
+    company: "Mundo IT",
+    period: "July 2023 - Present",
+    techs: [
+      "TypeScript",
+      "Next.js",
+      "React Native",
+      "Node.js",
+      "Express.js",
+      "MongoDB",
+      "PostgreSQL",
+      "Tailwind CSS",
+      "AWS",
+      "Git",
+    ],
+  },
+  {
+    jobTitle: "Backend Developer",
+    company: "AikoDev",
+    period: "January 2023 - June 2023",
+    techs: [
+      "TypeScript",
+      "Node.js",
+      "Express.js",
+      "MongoDB",
+      "MySQL",
+      "Git",
+      "NestJS",
+    ],
+  },
+  {
+    jobTitle: "Frontend Developer",
+    company: "Freelance",
+    period: "August 2022 - December 2023",
+    techs: [
+      "TypeScript",
+      "React.js",
+      "Next.js",
+      "React Native",
+      "Tailwind CSS",
+      "Node.js",
+      "Express.js",
+      "MongoDB",
+      "PostgreSQL",
+      "Git",
+      "AWS",
+    ],
+  },
+]
+
+export const educationDetails = [
+  {
+    title: "Electronic Engineering",
+    institution: "Universidad Nacional de Rosario",
+    period: "Mar 2018 - 2022",
+  },
+]
+
+export const certificationsDetails = [
+  {
+    title: "Software Developer",
+    institution: "Radium Rocket",
+    period: "Mar 2022 - Jul 2022",
+  },
+  {
+    title: "Design Patterns",
+    institution: "Educacion IT",
+    period: "Dec 2023",
+  },
+]
