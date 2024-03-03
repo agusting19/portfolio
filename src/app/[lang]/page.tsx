731713de88e1a98a1ce1dde7e951d1e1d35cd12f
Agustin Gomez <agusleongomez@gmail.com>
2024-03-02 21:34:54 -0300

    Refactor Home component to use translations

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,13 +1,23 @@
 import Experience from "~/_components/Experience"
 import Presentation from "~/_components/Presentation"
 import ProjectsList from "~/_components/ProjectsList"
+import { getDictionary } from "~/i18n/getDictionary"
+import { Locale } from "~/i18n/i18n-config"
 
-const Home = () => {
+interface HomeProps {
+  params: {
+    lang: Locale
+  }
+}
+
+const Home = async ({ params: { lang } }: HomeProps) => {
+  const { component } = await getDictionary(lang)
+
   return (
     <>
-      <Presentation />
-      <Experience />
-      <ProjectsList />
+      <Presentation translations={component.presentation} />
+      <Experience translations={component.experience} />
+      <ProjectsList tranlations={component.projects} />
     </>
   )
 }
