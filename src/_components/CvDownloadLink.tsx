614824422d5ede98de9c51ef0a42984c7d8bfb3a
Agustin Gomez <agusleongomez@gmail.com>
2024-03-02 21:34:29 -0300

    Refactor CvDownloadLink component to accept buttonText prop

--- src/_components/CvDownloadLink.tsx
+++ src/_components/CvDownloadLink.tsx
@@ -1,6 +1,10 @@
 import Link from "next/link"
 
-const CvDownloadLink = () => {
+interface CvDownloadLinkProps {
+  buttonText: string
+}
+
+const CvDownloadLink = ({ buttonText }: CvDownloadLinkProps) => {
   return (
     <Link
       href="/Gomez-Agustin.pdf"
@@ -8,7 +12,7 @@ const CvDownloadLink = () => {
       rel="noopener noreferrer"
       className="font-semibold py-4 px-8 block w-40 bg-transparent border-2 border-purple-600 transition-all duration-250 text-white text-sm cursor-pointer hover:shadow-[inset_0_0_0_2em_#a855f7]"
     >
-      Download CV
+      {buttonText}
     </Link>
   )
 }
