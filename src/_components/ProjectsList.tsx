a85b6a33353678788dd60e8b6b42e5741b63f945
Agustin Gomez <agusleongomez@gmail.com>
2024-03-02 21:34:39 -0300

    Refactor ProjectsList component to use translated data

--- src/_components/ProjectsList.tsx
+++ src/_components/ProjectsList.tsx
@@ -1,19 +1,26 @@
-import { projects } from "../constants"
+import { IMAGE_URLS } from "~/constants"
+import { Projects } from "~/types/i18n"
 import ProjectCard from "./ProjectCard"
 
-const ProjectsList = () => {
+interface ProjectsListProps {
+  tranlations: Projects
+}
+
+const ProjectsList = ({ tranlations }: ProjectsListProps) => {
+  const projectsList = tranlations.projectsList
+
   return (
     <section className="max-w-[1100px] py-16 px-auto bg-backgroundSecondary mx-auto md:py-20 md:px-16">
       <h2 className="text-4xl mb-6 text-white font-bold md:text-5xl">
-        Projects
+        {tranlations.title}
       </h2>
       <div className="flex flex-col justify-between gap-12 md:gap-20">
-        {projects.map((project, index) => {
+        {projectsList.map((project, index) => {
           return (
             <ProjectCard
               key={project.title}
               title={project.title}
-              imageUrl={project.imgURL}
+              imageUrl={IMAGE_URLS[index]}
               techs={project.techs}
               description={project.description}
               inverted={index % 2 !== 0}
